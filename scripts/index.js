3b65b08b4a26d0c45a886c8012474a76f4ce7297
Kirill Dolgikh <w1tchan@yandex.ru>
2025-04-25 23:15:33 +0300

    feat: реализован функционал функции добавления карточек и удаления

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsList = document.querySelector('.places__list');
+
+
+const createCard = (cardName, cardLink, deleteCard) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardContent = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardDeleteButton = cardContent.querySelector('.card__delete-button');
+  cardContent.querySelector('.card__title').textContent = cardName;
+  cardContent.querySelector('.card__image').src = cardLink;
+  
+  cardDeleteButton.addEventListener('click', () => deleteCard(cardContent));
+  
+  return cardContent;
+}
+
+const deleteCard = (card) => {
+  cardsList.removeChild(card);
+}
+
+initialCards.forEach((card) => {
+  cardsList.append(createCard(card.name, card.link, deleteCard));
+});
+
